474f3c504ec7ad7aedd8766833be950f59e3292d
Umukoro <faithumukorosmile@gmail.com>
2020-01-28 08:27:56 +0000

    initial Commit 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Xample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(5+8+3+2);
+        }
+    }
+}
